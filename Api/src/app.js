d374d7f2202e10fea3bcc91ac035730ead241efa
Andrea Sotil <andreasotil@ieholding.com>
2020-07-31 21:32:15 -0500

    api save

---
+++ Api/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json());
+
+//api routes
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+module.exports = app;
